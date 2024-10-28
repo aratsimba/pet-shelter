328e81d9253bcb61761d907a25db265b799573fe
AlainR <aratsimbazafy@cs.com>
2024-10-28 16:34:57 -0400

    Full starter app code

---
+++ src/components/AuthenticatedRoutes.jsx
@@ -0,0 +1,17 @@
+
+import { Outlet, Navigate } from 'react-router-dom';
+import { useAuthenticator } from "@aws-amplify/ui-react"
+
+
+function AuthenticatedRoutes() {
+    const { authStatus } = useAuthenticator(context => [context.authStatus]);
+
+    if (authStatus === 'authenticated' || authStatus === 'configuring') {
+        return <Outlet />
+    }
+
+    return <Navigate to="/login" />
+
+}
+
+export default AuthenticatedRoutes;
